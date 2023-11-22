fadb8ef673d1c549c38a318d135fc84b49142ae2
Artem Antonov <artfront88@gmail.com>
2023-11-22 14:24:35 +0200

    Updates

--- src/client/utils/common.js
+++
@@ -1,5 +0,0 @@
-export const getCollectionByUserId = (collection, userId) => collection.filter(item => item.userId === userId);
-
-export const getUsernameById = (users, id) => users.find(user => user.id === id).name;
-
-export const getFullAddress = user => `${user.address.zipcode}, ${user.address.street}, ${user.address.suite}, ${user.address.city}`;
